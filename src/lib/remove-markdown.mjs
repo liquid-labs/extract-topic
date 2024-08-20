9ebe6a882ab7bc83968776ebb0a23aeee9c97a9e
Zane Rockenbaugh <zane@liquid-labs.com>
2024-08-20 13:57:03 -0500

    add support for alt headers, blockquotes, and code blocks

--- src/lib/remove-markdown.mjs
+++ src/lib/remove-markdown.mjs
@@ -4,16 +4,22 @@ const inlineFormattersRe = /(__?_?|\*\*?\*?)(.*?)\1/gm
 const linkRe = /\[([^\]]*)\]\([^)]*\)/gm
 const backticksRe = /`(.*?)`/gm
 const headingMarkersRe = /^#+/gm
+const altMdHeaderRe = /^([^\n\r]+)(\n|\r|\r\n)([=-]{2,})(\n|\r|\r\n|$)/gm
 
 const removeMarkdown = (text, { keepWhitespace, removeBackticks }) => {
   // remove header markers (in case header retained)
   text = stripLeading('#+ ?', text, { keepWhitespace, noEscape : true })
+  text = text.replaceAll(altMdHeaderRe, '$1')
   // remove list formatting.
   text = stripLeading('* ', text, { keepWhitespace })
   text = stripLeading('- ', text, { keepWhitespace })
   text = stripLeading('(?:0|[1-9][0-9]*)\\. ', text, { keepWhitespace, noEscape : true })
+  // remove blockquotes
+  text = stripLeading('> ', text, { keepWhitespace })
   // remove inline formatting
   text = text.replaceAll(inlineFormattersRe, '$2')
+  // remove pre
+  text = text.replaceAll(/^\s*```[a-zA-Z0-9]*\s*$/gm, '')
   // remove links (and replace with label)
   text = text.replaceAll(linkRe, '$1')
   // remove backticks if told to
@@ -26,4 +32,4 @@ const removeMarkdown = (text, { keepWhitespace, removeBackticks }) => {
   return text
 }
 
-export { removeMarkdown }
+export { removeMarkdown, altMdHeaderRe }
