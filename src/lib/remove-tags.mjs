a090e6fa0300183f0e78f34b24201c183b603fc7
Zane Rockenbaugh <zane@liquid-labs.com>
2024-08-20 15:25:46 -0500

    decode html

--- src/lib/remove-tags.mjs
+++ src/lib/remove-tags.mjs
@@ -1,7 +1,11 @@
+import { decode } from 'he'
+
 const tagRe = /<[^\s>] ?[^\n\r>]*>/gm
 
 const removeTags = (text) => {
   text = text.replaceAll(tagRe, '')
+
+  text = decode(text)
 
   return text
 }
